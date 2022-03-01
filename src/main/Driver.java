456016cd5115a929f4601b69e4b3865e81b35d34
Abdul Hamid Umar <AbdulHamid.Umar@mitrais.com>
2022-03-01 16:20:35 +0800

    delete Inventory.java,  list of items moved into the driver code

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -2,10 +2,27 @@ package main;
 
 import main.Model.Item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Driver {
 
     public static void main(String[] args) {
+        List<Item> inventory = new ArrayList<>();
 
+        inventory.add(new Item("Book", 10, 10));
+        inventory.add(new Item("Phone", 10, 100));
 
     }
+    public static void buyItem(){}
+
+    public static void updateItem(){}
+
+    public static void manageItem(){}
+
+    public static void showItemList(){
+        System.out.println();
+    }
+
+
 }
--- src/main/Model/Item.java
+++ src/main/Model/Item.java
@@ -1,27 +1,40 @@
 package main.Model;
 
 public class Item {
-    private String itemName;
-    private int itemPoints;
+    String itemName;
+    int itemStock;
+    int itemPoints;
 
-    public Item(String itemName,  int itemPoints) {
+    public Item(String itemName, int itemStock, int itemPoints) {
         this.itemName = itemName;
+        this.itemStock = itemStock;
         this.itemPoints = itemPoints;
     }
 
     //function to buy item
     //parameter : Date, Item Name, Quantity
     public void buyItem(int date, String itemName, int qty){
+        if(this.itemStock > 0 && qty > 0 && qty <=this.itemStock){
+            setItemStock(this.itemStock - qty);
+            //record somewhere
+        } else {
+            System.out.println("Item sold out");
+        }
         //reduce item stock by quantity
         //record the date, item name, and quantity
     }
 
     //function to update item info
     //parameter : Item Name, Stock, Points
-    public void updateItem(String itemName, int itemPoints){
+    public void updateItem(String itemName, int itemStock, int itemPoints){
         setItemName(itemName);
+        setItemStock(itemStock);
         setItemPoints(itemPoints);
     }
+
+    //function to show history of item sales
+    //
+    public void showItemSalesHistory(){}
 
     public String getItemName() {
         return itemName;
@@ -29,6 +42,14 @@ public class Item {
 
     public void setItemName(String itemName) {
         this.itemName = itemName;
+    }
+
+    public int getItemStock() {
+        return itemStock;
+    }
+
+    public void setItemStock(int itemStock) {
+        this.itemStock = itemStock;
     }
 
     public int getItemPoints() {
@@ -43,6 +64,7 @@ public class Item {
     public String toString() {
         return "main.Model.Carrot{" +
                 "itemName='" + itemName + '\'' +
+                ", itemStock=" + itemStock +
                 ", itemPoints=" + itemPoints +
                 '}';
     }
