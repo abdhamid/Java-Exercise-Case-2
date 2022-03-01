8a6fb3b6572e704b7d867e199d8b1fa2cbedb7cf
Abdul Hamid Umar <AbdulHamid.Umar@mitrais.com>
2022-03-01 18:13:16 +0800

    Added console menu

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -4,25 +4,87 @@ import main.Model.Item;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Driver {
+    static void menu() {
+        System.out.println("1 - Add Item");
+        System.out.println("2 - Update Item");
+        System.out.println("3 - Display Items");
+        System.out.println("0 - Exit Program");
+    }
 
     public static void main(String[] args) {
         List<Item> inventory = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int pointer; //For navigating the menu
 
-        inventory.add(new Item("Book", 10, 10));
-        inventory.add(new Item("Phone", 10, 100));
+        do {
+            menu();
+            pointer = scanner.nextInt();
+            switch (pointer) {
+                case 1 -> {
+                    System.out.println("Enter item Name :");
+                    String name = scanner.next();
+                    System.out.println("Enter item stock :");
+                    int itemStock  = scanner.nextInt();
+                    System.out.println("Enter item points");
+                    int itemPoint = scanner.nextInt();
+                    addItem(inventory, name, itemStock,itemPoint);
+//                    addItem(inventory, "Phone", 100,10);
+                    System.out.println("Item Added");
+                }
+                case 2 -> {
+                    System.out.println("Enter item name :");
+                    String name = scanner.next();
+                    System.out.println("Enter new item stock :");
+                    int itemStock  = scanner.nextInt();
+                    System.out.println("Enter new item points");
+                    int itemPoint = scanner.nextInt();
+                    updateItem(inventory, name, itemStock,itemPoint);
+//                    updateItem(inventory, "Phone", 50,30);
+//                    System.out.println("Item Added");
+                }
+                case 3 -> displayItems(inventory);
+
+            }
+        } while (pointer != 0);
 
     }
-    public static void buyItem(){}
 
-    public static void updateItem(){}
-
-    public static void manageItem(){}
-
-    public static void showItemList(){
-        System.out.println();
+    public static void addItem(List<Item> inventory, String itemName, int itemStock, int itemPoint){
+        inventory.add(new Item(itemName, itemStock, itemPoint));
     }
+
+    public static void displayItems(List<Item> inventory){
+        for (Item item: inventory) {
+            System.out.println(item);
+        }
+    }
+
+    public static void updateItem(List<Item> inventory, String itemName, int itemStock, int itemPoint){
+        for(Item item : inventory){
+            if(itemName.equals(item.getItemName())){
+                System.out.println("Item name   : " + item.getItemName());
+//                item.setItemName(itemName);
+                System.out.println("Stock Changed   : " + item.getItemStock() +"    =>  " + itemStock);
+                item.setItemStock(itemStock);
+                System.out.println("Points Changed  : " + item.getItemPoints() +"   =>  " + itemPoint);
+                item.setItemPoints(itemPoint);
+//                System.out.println(item);
+            } else {
+                System.out.println("Item not found");
+            }
+        }
+    }
+
+//    public static void manageItem(String itemName){
+//
+//    }
+
+    public static void showItemList(){}
+
+    public static void showItemSold(){}
 
 
 }
--- src/main/Model/Item.java
+++ src/main/Model/Item.java
@@ -11,31 +11,6 @@ public class Item {
         this.itemPoints = itemPoints;
     }
 
-    //function to buy item
-    //parameter : Date, Item Name, Quantity
-    public void buyItem(int date, String itemName, int qty){
-        if(this.itemStock > 0 && qty > 0 && qty <=this.itemStock){
-            setItemStock(this.itemStock - qty);
-            //record somewhere
-        } else {
-            System.out.println("Item sold out");
-        }
-        //reduce item stock by quantity
-        //record the date, item name, and quantity
-    }
-
-    //function to update item info
-    //parameter : Item Name, Stock, Points
-    public void updateItem(String itemName, int itemStock, int itemPoints){
-        setItemName(itemName);
-        setItemStock(itemStock);
-        setItemPoints(itemPoints);
-    }
-
-    //function to show history of item sales
-    //
-    public void showItemSalesHistory(){}
-
     public String getItemName() {
         return itemName;
     }
@@ -62,10 +37,9 @@ public class Item {
 
     @Override
     public String toString() {
-        return "main.Model.Carrot{" +
-                "itemName='" + itemName + '\'' +
-                ", itemStock=" + itemStock +
-                ", itemPoints=" + itemPoints +
-                '}';
+        return  "================================\n" +
+                "Item\t\t" + "Stock\t\t" + "Points" + "\n" +
+                "================================\n" +
+                String.format("%s%10d%10d", itemName, itemStock, itemPoints);
     }
 }
